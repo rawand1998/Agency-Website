68a5994257e5e5e1a109ed49ea60736f337db3a3
rawand1998 <rawandgaradh@gmail.com>
2022-11-13 13:26:05 +0200

    header cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import hero_img from "../images/hero-img.png";
 function Header() {
+  const list = [
+    { name: "Clients", no: "5K" },
+    { name: "Running Project", no: "350+" },
+    { name: "Running Completed", no: "900+" },
+  ];
   return (
-    <div className="header_container">
-      <div className="header_content">
-        <h2>
-          We`re create perfect <br />
-          digiatl profuct to <br />
-          <span>promot your brand</span>
-        </h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.{" "}
-        </p>
-        <div className="header_btns">
-          <a href="">Get Start Now</a>
-          <a href="">Dawnload Now</a>
+    <>
+      <div className="header_container">
+        <div className="header_content">
+          <h2>
+            We`re create perfect <br />
+            digiatl profuct to <br />
+            <span>promot your brand</span>
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.{" "}
+          </p>
+          <div className="header_btns">
+            <a href="">Get Start Now</a>
+            <a href="">Dawnload Now</a>
+          </div>
+        </div>
+
+        <div className="hero_img">
+          <img src={hero_img} alt="" />
         </div>
       </div>
 
-      <div className="hero_img">
-        <img src={hero_img} alt="" />
+      <div className="header_cards">
+        {list.map(({name,no})=>(
+            <div className="card">
+               
+                <span>{no}</span>
+                <span>{name}</span>
+            </div>
+        ))}
       </div>
-    </div>
+    </>
   );
 }
 
