af3b71e43e4c4d1ae170bc238cf07aaa0d841e44
rawand1998 <rawandgaradh@gmail.com>
2022-11-13 13:04:30 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/index'
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Header/>
     </div>
   );
 }
