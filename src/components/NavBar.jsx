228551c3edfbde97f7519d149db82e9ffb74a808
rawand1998 <rawandgaradh@gmail.com>
2022-11-13 12:19:28 +0200

    navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function NavBar() {
   const list = [
@@ -8,22 +8,48 @@ function NavBar() {
     { name: "Projects" },
     { name: "Blog" },
   ];
+  const scrollNav = useRef();
+  const showMenu = useRef();
+  const showLogin = useRef();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        scrollNav.current.classList.add("header_shrink");
+      } else {
+        scrollNav.current.classList.remove("header_shrink");
+      }
+    });
+
+    // return () => window.removeEventListener("scroll");
+  }, []);
+  const showMenuList = () => {
+    showMenu.current.classList.toggle("show_menu");
+    showLogin.current.classList.toggle("show_login");
+  };
   return (
-    <div className="navbar_conatiner">
+    <div ref={scrollNav} className="navbar_conatiner">
       <div className="logo">
         {" "}
         <span>Dencey</span>
       </div>
-      <nav>
+      <nav ref={showMenu}>
         {list.map(({ name }) => (
           <li>
             <a href="">{name}</a>
           </li>
         ))}
       </nav>
-      <div className="login_icon">
+      <div className="login_icon" ref={showLogin}>
         <i class="ri-login-box-line"></i>
         <span>Login</span>
+      </div>
+
+      <div className="mobile_menu" onClick={showMenuList}>
+        <i class="ri-menu-line"></i>
       </div>
     </div>
   );
